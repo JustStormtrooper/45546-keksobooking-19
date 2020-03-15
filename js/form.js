992c295c9f8abcf4ca2af639a5e7d95e9ccb1ffe
Dmitry <d.volgushev@yandex.ru>
2020-03-16 00:00:53 +0300

    Добавление перемещения главной метки.

--- js/form.js
+++ js/form.js
@@ -1,15 +1,9 @@
 'use strict';
 
 (function () {
-
-  var Y_LOCATION_RANGE = [130, 630];
-  var MAP_PIN_MAIN_HEIGHT_ADD = 22;
   var OFFER_TYPES_MIN_PRICE = {flat: 1000, bungalo: 0, house: 5000, palace: 10000};
 
-  var mapElement = document.querySelector('.map');
-  var mapPinsElement = document.querySelector('.map__pins');
   var adFormElement = document.querySelector('.ad-form');
-  var mapPinMainElement = mapElement.querySelector('.map__pin--main');
   var adFormAddressElement = adFormElement.querySelector('input[name=address]');
   var adFormRoomsElement = adFormElement.querySelector('select[name=rooms]');
   var adFormGuestsElement = adFormElement.querySelector('select[name=capacity]');
@@ -17,10 +11,6 @@
   var adFormPriceElement = adFormElement.querySelector('input[name=price]');
   var adFormTimeinElement = adFormElement.querySelector('select[name=timein]');
   var adFormTimeoutElement = adFormElement.querySelector('select[name=timeout]');
-  var mapPinsFieldWidth = mapPinsElement.offsetWidth;
-  var mapPinMainWidth = mapPinMainElement.offsetWidth;
-  var mapPinMainHeight = mapPinMainElement.offsetHeight;
-  var mapPinMainHeightActive = mapPinMainHeight + MAP_PIN_MAIN_HEIGHT_ADD;
 
   adFormAddressElement.readOnly = true;
 
@@ -36,21 +26,8 @@
   }
 
   function fillAddressField(x, y, pageState) {
-    var newX = Math.round(x + mapPinMainWidth / 2);
-    var newY = Math.round(y + (pageState ? mapPinMainHeightActive : mapPinMainHeight / 2));
-
-    if (newX < 0) {
-      newX = 0;
-    } else if (newX > mapPinsFieldWidth) {
-      newX = mapPinsFieldWidth;
-    }
-
-    if (newY < Y_LOCATION_RANGE[0]) {
-      newY = Y_LOCATION_RANGE[0];
-    } else if (newY > Y_LOCATION_RANGE[1]) {
-      newY = Y_LOCATION_RANGE[1];
-    }
-
+    var newX = Math.round(x + window.constants.MAP_PIN_MAIN_WIDTH / 2);
+    var newY = Math.round(y + (pageState ? window.constants.MAP_PIN_MAIN_HEIGHT_ACTIVE : window.constants.MAP_PIN_MAIN_HEIGHT / 2));
     adFormAddressElement.value = newX + ', ' + newY;
   }
 
--- js/map.js
+++ js/map.js
@@ -1,6 +1,7 @@
 'use strict';
 
 (function () {
+  var Y_LOCATION_RANGE = [130, 630];
 
   var mapElement = document.querySelector('.map');
   var mapFiltersElement = mapElement.querySelector('.map__filters');
@@ -8,6 +9,12 @@
   var mapPinMainElement = mapElement.querySelector('.map__pin--main');
   var mapPinMainLeft = parseInt(mapPinMainElement.style.left, 10);
   var mapPinMainTop = parseInt(mapPinMainElement.style.top, 10);
+  var mapPinsFieldWidth = mapElement.offsetWidth;
+
+  var mapPinMainCoordLimits = {
+    x: [-window.constants.MAP_PIN_MAIN_WIDTH / 2, mapPinsFieldWidth - window.constants.MAP_PIN_MAIN_WIDTH / 2],
+    y: [Y_LOCATION_RANGE[0] - window.constants.MAP_PIN_MAIN_HEIGHT_ACTIVE, Y_LOCATION_RANGE[1] - window.constants.MAP_PIN_MAIN_HEIGHT_ACTIVE]
+  };
 
   function showOfferMap() {
     mapElement.classList.remove('map--faded');
@@ -39,6 +46,68 @@
 
   mapPinMainElement.addEventListener('mousedown', setPageActive);
   mapPinMainElement.addEventListener('click', setPageActive);
+
+  mapPinMainElement.addEventListener('mousedown', function (evt) {
+
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var shift = {
+      x: startCoords.x,
+      y: startCoords.y
+    };
+
+    function onMouseMove(moveEvt) {
+      updateMapPinMainCoords(moveEvt.clientX, moveEvt.clientY);
+    }
+
+    function onMouseUp(upEvt) {
+      updateMapPinMainCoords(upEvt.clientX, upEvt.clientY);// ?????
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    }
+
+    function updateMapPinMainCoords(x, y) {
+      trackMouseMovement(x, y);
+
+      var mapPinMainNewX = mapPinMainElement.offsetLeft - shift.x;
+      var mapPinMainNewY = mapPinMainElement.offsetTop - shift.y;
+
+      if (mapPinMainNewX < mapPinMainCoordLimits.x[0]) {
+        mapPinMainNewX = mapPinMainCoordLimits.x[0];
+      } else if (mapPinMainNewX > mapPinMainCoordLimits.x[1]) {
+        mapPinMainNewX = mapPinMainCoordLimits.x[1];
+      }
+
+      if (mapPinMainNewY < mapPinMainCoordLimits.y[0]) {
+        mapPinMainNewY = mapPinMainCoordLimits.y[0];
+      } else if (mapPinMainNewY > mapPinMainCoordLimits.y[1]) {
+        mapPinMainNewY = mapPinMainCoordLimits.y[1];
+      }
+
+      mapPinMainElement.style.left = mapPinMainNewX + 'px';
+      mapPinMainElement.style.top = mapPinMainNewY + 'px';
+      window.form.fillAddressField(mapPinMainNewX, mapPinMainNewY, true);
+    }
+
+    function trackMouseMovement(newMouseX, newMouseY) {
+      shift = {
+        x: startCoords.x - newMouseX,
+        y: startCoords.y - newMouseY
+      };
+
+      startCoords = {
+        x: newMouseX,
+        y: newMouseY
+      };
+    }
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+
+  });
 
   setPageInactive();
 
